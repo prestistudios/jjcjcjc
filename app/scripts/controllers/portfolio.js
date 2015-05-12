ecca68df9ae915a5f92896ea8c0f0e3de5a61e9b
Jean-Christophe Therien <prestistudios@gmail.com>
2015-05-12 02:10:38 -0400

    update2
    views
    
    des views de plus

---
+++ app/scripts/controllers/portfolio.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name 2App.controller:PortfolioCtrl
+ * @description
+ * # PortfolioCtrl
+ * Controller of the 2App
+ */
+angular.module('2App')
+  .controller('PortfolioCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
